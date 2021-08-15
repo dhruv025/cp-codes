e0c0670de7f7e813cc319d0ae51961d13995be7a
Dhruv Goel <40907067+dhruv025@users.noreply.github.com>
2021-08-15 15:54:48 +0530

    Update 132.cpp

--- 132.cpp
+++ 132.cpp
@@ -1,3 +1,5 @@
+/*1st approach
+Time Complexity - O(n^3)*/
 class Solution{
 public:
 	bool recreationalSpot(int arr[], int n)
@@ -14,6 +16,28 @@ public:
 		        }
 		    }
 		}                      
+		return false;
+	}
+};
+
+
+/*2nd approach
+Time Complexity - O(n^2)*/
+class Solution{
+public:
+	bool recreationalSpot(int arr[], int n)
+	{
+		// Your code goes here 
+		int leftmin = arr[0];
+		for(int j = 1; j < n-1; ++j)
+		{
+		    for(int k = j+1; k < n; ++k)
+		    {
+		        if(arr[k] > leftmin && arr[k] < arr[j])
+		            return true;
+		    }
+		    leftmin = min(leftmin,arr[j]);
+		}
 		return false;
 	}
 };
