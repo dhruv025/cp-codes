4400a8668ae5cbb923bf1d632145915fcd047494
Dhruv Goel <40907067+dhruv025@users.noreply.github.com>
2021-08-19 18:38:28 +0530

    Update clone_linked_list.cpp

--- clone_linked_list.cpp
+++ clone_linked_list.cpp
@@ -28,3 +28,36 @@ Node *copyList(Node *head)
     }
     return newhead;
 }
+
+/*1st approach
+Time Complexity - O(n)
+Space Complexity - O(1)*/
+Node *copyList(Node *head)
+{
+    //Write your code here
+    Node* newhead, *temp, *node ,*original, *copy;
+    temp = head;
+    int c = 0;
+    while(temp)
+    {
+        node = new Node(temp->data);
+        node->next = temp->next;
+        temp->next = node;
+        temp = temp->next->next;
+    }
+    temp = head;
+    while(temp)
+    {
+        temp->next->arb = temp->arb ? temp->arb->next : temp->arb;
+        temp = temp->next->next;
+    }
+    original = head, newhead = copy = head->next;
+    while(original && copy)
+    {
+        original->next = original->next->next;
+        original = original->next;
+        copy->next = copy->next ? copy->next->next : copy->next;
+        copy = copy->next;
+    }
+    return newhead;
+}
